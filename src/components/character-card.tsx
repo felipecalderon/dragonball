9aa7d40e3aa2df28935a5ccc580e65c28b489b4e
Felipe <felipe.calderon321@gmail.com>
2024-11-06 16:45:52 -0300

    Refactor CharacterCard layout: reposition power displays for improved visibility and overall presentation of character details

--- src/components/character-card.tsx
+++ src/components/character-card.tsx
@@ -10,21 +10,19 @@ export default function CharacterCard({ character }: { character: Character }) {
         <MagicCard className='px-6 py-3 text-center size-auto flex flex-col justify-between relative'>
             <Image src={character.image} alt={character.name} width={150} height={150} className='mx-auto w-fit max-h-40 drop-shadow-2xl' />
             <p className='text-xl font-black'>{character.name}</p>
-            <p className='text-sm'>
-                Poder: <span className='text-base font-semibold'>{formatKi(parseKi(character.ki))}</span>
-            </p>
-            <p className='text-sm'>
-                Max poder: <span className='text-base font-semibold'>{formatKi(parseKi(character.maxKi))}</span>
-            </p>
             <p className='text-xs italic mx-auto w-2/3 pt-3'>
                 {character.description.length > 150 ? character.description.slice(0, 150) + '...' : character.description}
             </p>
             <p className='absolute top-0 left-0 text-white'>
                 <GenderSymbol gender={character.gender} />
             </p>
-            <p className='text-xs absolute top-0 right-0'>{character.affiliation}</p>
             <FloatingImage race={character.race} />
-            <p className='text-xs'>{character.originPlanet}</p>
+            <p className='text-sm absolute bottom-0 left-0'>
+                Poder: <span className='text-base font-semibold'>{formatKi(parseKi(character.ki))}</span>
+            </p>
+            <p className='text-sm absolute bottom-0 right-0'>
+                Max poder: <span className='text-base font-semibold'>{formatKi(parseKi(character.maxKi))}</span>
+            </p>
         </MagicCard>
     )
 }
